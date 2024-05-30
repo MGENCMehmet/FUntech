605b6dab94e761a5275ee6c1e1bce17d93d96424
MGENCMehmet <162979624+MGENCMehmet@users.noreply.github.com>
2024-05-30 21:53:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,8 @@ import pandas as pd
 import pandas_ta as ta
 
 st.set_page_config(layout="wide")
-
-col1, col2 = st.columns([1, 1])
-
 st.title("FUntech")
+col1, col2 = st.columns([1, 1])
 
 col1.subheader("Panel")
 
