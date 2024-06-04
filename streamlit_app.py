a26d3fe72cd7e08ca8fd68a5a9dd149440809506
MGENCMehmet <162979624+MGENCMehmet@users.noreply.github.com>
2024-06-04 20:42:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -269,7 +269,7 @@ else:
             x_test = np.array(x_test)                                                                                                                     
                                                                                                                                                           
             x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))                                                                            
-            preds = model.predict(x_test)                                                                                                                 
+            preds = model.predict(x_test, verbose=0)                                                                                                                 
             preds = mms.inverse_transform(preds)                                                                                                          
             train = df[:df_train_len]                                                                                                                     
             valid = df[df_train_len:]                                                                                                                     
