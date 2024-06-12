dc097032fa0e1b80e25dbdabb49eaf806b993173
MGENCMehmet <162979624+MGENCMehmet@users.noreply.github.com>
2024-06-12 21:31:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -318,7 +318,7 @@ elif page == "Tahmin":
       x_train, y_train = np.array(x_train), np.array(y_train)
       x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
       
-      if col2.button('Tahmin Et'):
+      if st.button('Tahmin Et'):
           with st.spinner('Tahmin Ediliyor...'):
               model = Sequential()
               model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1)))
