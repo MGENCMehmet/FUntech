e0e7f7a67f7f15a70af0b44e009db85830cc0841
MGENCMehmet <162979624+MGENCMehmet@users.noreply.github.com>
2025-05-29 22:19:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -360,10 +360,12 @@ elif page == "Tahmin":
               valid = df[df_train_len:]
               preds = pd.Series(index=valid.index, data=preds.reshape(1, len(preds))[0])
 
-              figp = px.line(x=train.index, y=train.values, title=f'{ticker}  için Tahminlerimiz')
+              ticker_name = ticker[0]
+
+              figp = px.line(train, title=f'{ticker_name} için Tahminlerimiz')
               figp.add_trace(go.Scatter(x=valid.index, y=valid.values, mode='lines', name='Valid', line=dict(color='orange')))
               figp.add_trace(go.Scatter(x=valid.index, y=preds, mode='lines', name='Preds', line=dict(color='green')))
 
-              st.plotly_chart(figp)
+st.plotly_chart(figp)
     else:
       st.write("Bu özelliği kullanabilmek için 1 hisse seçiniz")
